5e199d4bb6ef8a26f5a31a83ff46047290927e2d
James Coffman <jamesc4023@gmail.com>
2022-05-28 10:56:10 -0400

    commit tree 09e2e58ae6b2a5b7499aa01164bcc95ef84dd3ce

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < Sinatra::Base
+  
+  set :default_content_type, 'application/json'
 
   get '/' do
     { message: "Hello world" }.to_json
   end
+  
+  get '/games' do 
+    # get's all the games from the db then returns them as JSON 
+    Game.all.order(:title).limit(10).to_json
+  end
 
+  get '/games/:id' do
+    # finds the game with the id provided from params and returns it as JSON
+    # Game.find(params[:id]).to_json(include: {reviews: { include: :user } })
+    game = Game.find(params[:id])
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      }}
+    })
+  end
 end
